f5d179f376208c383d1970ae2a4bc714f0f376c6
david-binda <david.binovec@gmail.com>
2017-03-21 15:02:43 +0100

    Plugin scaffolding

---
+++ README.md
@@ -0,0 +1,3 @@
+== Cronify Term Count Update ==
+
+This plugin deferres the _update_term_count_on_transition_post_status action to cron
---
+++ cronify-term-count-update.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Cronify Term Count Update
+ * Plugin URI:      https://github.com/Automattic/cronify-term-count-update
+ * Description:     Deferres the _update_term_count_on_transition_post_status action to cron
+ * Author:          Automattic
+ * Author URI:      https://automattic.com/
+ * Text Domain:     cronify-term-count-update
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cronify_Term_Count_Update
+ */
