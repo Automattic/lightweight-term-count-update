7e8ff51e38a17c3e3243356d3fa23233d651c16d
David Binovec <david.binovec@gmail.com>
2017-03-24 17:00:55 +0000

    Adding first batch of WordPress unit tests
    They all failing for now, as the counting seems to be off by 1 when it comes to incrementing and decrementing seems to be off as well

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -10,11 +10,77 @@
  */
 class SampleTest extends WP_UnitTestCase {
 
+
 	/**
-	 * A single example test.
+	 * Test a single post publish action with a single category
 	 */
-	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+	function test_single_post_publish_with_a_single_category() {
+		
+		// create Test Category
+		$testcat = self::factory()->category->create_and_get(
+			array(
+				'slug' => 'mytestcat',
+				'name' => 'My Test Category 1',
+			)
+		);
+
+		$post_id = self::factory()->post->create( array(
+			'post_type' => 'post',
+			'post_title' => 'Test post 1',
+			'post_status' => 'publish',
+			'post_category' => array( $testcat->term_id ),
+		) );
+
+		$category = get_term( $testcat->term_id, 'category' );
+		
+		$this->assertEquals( $testcat->count + 1, $category->count );
+	}
+
+	/**
+	 * Test multiple posts counting
+	 */
+	function test_multiple_posts_counting() {
+		// Create a test category.
+		$testcat = self::factory()->category->create_and_get(
+			array(
+				'slug' => 'mytestcat',
+				'name' => 'My Test Category 1',
+			)
+		);
+
+		self::factory()->post->create_many( 3, array(
+			'post_type' => 'post',
+			'post_status' => 'publish',
+			'post_category' => array( $testcat->term_id ),
+		) );
+
+		$category = get_term( $testcat->term_id, 'category' );
+
+		$this->assertEquals( $testcat->count + 3, $category->count );
+	}
+
+	/**
+	 * Test post unpublish action
+	 */
+	function test_post_unpublish() {
+		$testcat = self::factory()->category->create_and_get(
+			array(
+				'slug' => 'mytestcat',
+				'name' => 'My Test Category 1',
+			)
+		);
+
+		$post_id = self::factory()->post->create( array(
+			'post_type' => 'post',
+			'post_status' => 'publish',
+			'post_category' => array( $testcat->term_id ),
+		) );
+
+		wp_update_post( array( 'ID' => $post_id, 'post_status' => 'draft' ) );
+
+		$category = get_term( $testcat->term_id, 'category' );
+
+		$this->assertEquals( $testcat->count, $category->count );
+
 	}
 }
