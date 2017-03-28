f3a4c88d089adc6231cac514612d3a6bab734bf5
Matt Boynes <mboynes@alleyinteractive.com>
2017-03-27 21:43:39 -0400

    Account for term relationship changes

--- lightweight-term-count-update.php
+++ lightweight-term-count-update.php
@@ -24,6 +24,14 @@ class LTCU_Plugin {
 	public $counted_statuses = array( 'publish' );
 
 	/**
+	 * Store the terms that have been incremented or decremented to avoid
+	 * duplicated efforts.
+	 *
+	 * @var array
+	 */
+	public $counted_terms = array();
+
+	/**
 	 * Build an instance of the class. Helper to allow the class to be
 	 * instantiated via a hook.
 	 *
@@ -38,41 +46,61 @@ class LTCU_Plugin {
 	}
 
 	public function init() {
+		// Prevent core from counting terms.
+		wp_defer_term_counting( true );
 		remove_action( 'transition_post_status', '_update_term_count_on_transition_post_status' );
+
 		$this->counted_statuses = apply_filters( 'ltcu_counted_statuses', $this->counted_statuses );
+		add_action( 'transition_post_status', array( $this, 'transition_post_status' ), 10, 3 );
+		add_action( 'added_term_relationship', array( $this, 'added_term_relationship' ), 10, 3 );
+		add_action( 'deleted_term_relationships', array( $this, 'deleted_term_relationships' ), 10, 3 );
 	}
 
-	public function quick_update_terms_count( $new, $old, $post ) {
-		global $wpdb;
+	/**
+	 * When a term relationship is added, increment the term count.
+	 *
+	 * @param int    $object_id Object ID.
+	 * @param int    $tt_id     Single term taxonomy ID.
+	 * @param string $taxonomy  Taxonomy slug.
+	 */
+	public function added_term_relationship( $object_id, $tt_id, $taxonomy ) {
+		$post = get_post( $object_id );
+		$this->quick_update_terms_count( $post, (array) $tt_id, $taxonomy, 'increment' );
+	}
 
-		$transition_type = $this->transition_type( $new, $old );
+	/**
+	 * When a term relationship is deleted, decrement the term count.
+	 *
+	 * @param int    $object_id Object ID.
+	 * @param array  $tt_ids    Array of term taxonomy IDs.
+	 * @param string $taxonomy  Taxonomy slug.
+	 */
+	public function deleted_term_relationships( $object_id, $tt_ids, $taxonomy ) {
+		$post = get_post( $object_id );
+		$this->quick_update_terms_count( $post, $tt_ids, $taxonomy, 'decrement' );
+	}
 
-		if ( ! $transition_type ) {
-			return false;
-		}
-
-		foreach ( (array) get_object_taxonomies( $post->post_type, 'object' ) as $tax ) {
-			$tt_ids = wp_get_object_terms( $post->ID, $tax->name, array(
+	/**
+	 * When a post transitions, increment or decrement term counts as
+	 * appropriate.
+	 *
+	 * @param  string   $new_status New post status.
+	 * @param  string   $old_status Old post status.
+	 * @param  \WP_Post $post       Post being transitioned.
+	 */
+	public function transition_post_status( $new_status, $old_status, $post ) {
+		foreach ( (array) get_object_taxonomies( $post->post_type ) as $taxonomy ) {
+			$tt_ids = wp_get_object_terms( $post->ID, $taxonomy, array(
 				'fields' => 'tt_ids',
 			) );
 
-			if ( is_array( $tt_ids ) ) {
-				$tt_ids = array_map( 'intval', $tt_ids );
-				// Respect if a taxonomy has a callback override.
-				if ( ! empty( $tax->update_count_callback ) ) {
-					call_user_func( $tax->update_count_callback, $tt_ids, $tax->name );
-				} elseif ( $tt_ids ) {
-					$tt_ids_string = '(' . implode( ',', $tt_ids ) . ')';
-					if ( $transition_type === 'increment' ) {
-						// Incrementing
-						$update_query = "UPDATE {$wpdb->term_taxonomy} AS tt SET tt.count = tt.count + 1 WHERE tt.term_taxonomy_id IN $tt_ids_string";
-					} else {
-						// Decrementing
-						$update_query = "UPDATE {$wpdb->term_taxonomy} AS tt SET tt.count = tt.count - 1 WHERE tt.term_taxonomy_id IN $tt_ids_string AND tt.count > 0";
-					}
-					$wpdb->query( $update_query );
-				}
-				clean_term_cache( $tt_ids, '', false );
+			if ( ! empty( $tt_ids ) ) {
+				$this->quick_update_terms_count(
+					$post,
+					$tt_ids,
+					$taxonomy,
+					$this->transition_type( $new_status, $old_status )
+				);
 			}
 		}
 
@@ -87,12 +115,67 @@ class LTCU_Plugin {
 
 			if ( $attachments->have_posts() ) {
 				foreach ( $attachments->posts as $post ) {
-					$this->quick_update_terms_count( $new, $old, $post );
+					$this->transition_post_status( $new_status, $old_status, $post );
 				}
 			}
 		}
 	}
 
+	/**
+	 * Update term counts using a very light SQL query.
+	 *
+	 * @param  \WP_Post $post            Post with the term relationship.
+	 * @param  array    $tt_ids          Term taxonomy IDs.
+	 * @param  string   $taxonomy        Taxonomy slug.
+	 * @param  string   $transition_type 'increment' or 'decrement'.
+	 */
+	public function quick_update_terms_count( $post, $tt_ids, $taxonomy, $transition_type ) {
+		global $wpdb;
+
+		$tax_obj = get_taxonomy( $taxonomy );
+		if ( $tax_obj ) {
+			$tt_ids = array_filter( array_map( 'intval', (array) $tt_ids ) );
+
+			// Respect if a taxonomy has a callback override.
+			if ( ! empty( $tax_obj->update_count_callback ) ) {
+				call_user_func( $tax_obj->update_count_callback, $tt_ids, $tax_obj->name );
+			} elseif ( ! empty( $tt_ids ) ) {
+				$tt_ids_string = '(' . implode( ',', $tt_ids ) . ')';
+				if ( ! isset( $this->counted_terms[ $post->ID ][ $taxonomy ][ $transition_type ] ) ) {
+					$this->counted_terms[ $post->ID ][ $taxonomy ][ $transition_type ] = array();
+				}
+
+				// Ensure that these terms haven't already been counted.
+				$tt_ids = array_diff( $tt_ids, $this->counted_terms[ $post->ID ][ $taxonomy ][ $transition_type ] );
+
+				if ( ! empty( $tt_ids ) ) {
+					$this->counted_terms[ $post->ID ][ $taxonomy ][ $transition_type ] = array_merge(
+						$this->counted_terms[ $post->ID ][ $taxonomy ][ $transition_type ],
+						$tt_ids
+					);
+
+					if ( $transition_type === 'increment' ) {
+						// Incrementing
+						$update_query = "UPDATE {$wpdb->term_taxonomy} AS tt SET tt.count = tt.count + 1 WHERE tt.term_taxonomy_id IN $tt_ids_string";
+					} else {
+						// Decrementing
+						$update_query = "UPDATE {$wpdb->term_taxonomy} AS tt SET tt.count = tt.count - 1 WHERE tt.term_taxonomy_id IN $tt_ids_string AND tt.count > 0";
+					}
+					$wpdb->query( $update_query );
+				}
+			}
+
+			clean_term_cache( $tt_ids, $taxonomy, false );
+		}
+	}
+
+	/**
+	 * Determine if a term count should be incremented or decremented.
+	 *
+	 * @param  string      $new New post status.
+	 * @param  string      $old Old post status.
+	 * @return string|bool 'increment', 'decrement', or false.
+	 */
 	public function transition_type( $new, $old ) {
 		if ( ! is_array( $this->counted_statuses ) || ! $this->counted_statuses ) {
 			return false;
